75aeecab6e6a626544e39e880007b9a558867867
SoapingYou <88288006+SoapingYou@users.noreply.github.com>
2026-07-27 19:19:25 -0400

    add things
    six seven

--- debug_funcs.py
+++ debug_funcs.py
@@ -1,28 +1,33 @@
 #import functions
-
+from phaseSolutions import phasesToLocation1D
 #import dependencies
 import numpy as np
 import math 
 
 
-def phasesToLocation1Dtest(optional_inputs=None, trial_num = 100,
+def phasesToLocation1Dtest(trial_num, optional_inputs=None,
                            PRECISION=0.8, TRY_LIMIT=5,  
-                           n=5,low=25,high=100):
+                           n=5,low=25,high=100, threshold_pi=0.15,
+                           verbose=False):
     '''
     Tests phasesToLocation1D 
 
+     - param trial_num is # of trial nums,      ALWAYS INCLUDE THIS!!!
+       either consistent with optional_inputs, 
+       or # of random trials to run
     - param optional_inputs is either be None, 
        or a numpy array of shape (trial_num,n,2),
        or a numpy array of shape (n,2) if trial_num is 1
-    - param trial_num is # of trial nums, 
-       either consistent with optional_inputs, 
-       or # of random trials to run
+    - 
     - param n is # of modules given
     - param low is the lowest si that can be generated
     - param high is the highest si that can be generated
+    - param verbose details correct 
     '''
+
+    #Assign training input to optional input if applicable
     _generateNewInput = False
-    if optional_inputs == None:
+    if optional_inputs is None:
         _generateNewInput = True
     elif not isinstance(optional_inputs, np.ndarray):
         raise ValueError("Please pass optional_inputs as a numpy array")
@@ -35,28 +40,42 @@ def phasesToLocation1Dtest(optional_inputs=None, trial_num = 100,
     else:
         training_input = optional_inputs
 
+    #Create training input 
     if(_generateNewInput):
         rng = np.random.default_rng(seed=42)
-        test_inputs = np.zeros((trial_num,n,2))
+        training_input = np.zeros((trial_num,n,2))
         for i in range(trial_num):
-            _si = rng.integers(low,high,size=n)
+            _si = rng.choice(np.arange(low,high),size=n, replace=False)
             _pi = rng.random(size=5) * 2 * np.pi
             training_input[i,:,0] = _si
             training_input[i,:,1] = _pi
-
-    
+    print(training_input[0])    
+    #Test training input
     for i in range(trial_num):
         solution = phasesToLocation1D(data=training_input[i], 
                            PRECISION=PRECISION, 
                            TRY_LIMIT=TRY_LIMIT)
         #check if solution works
         for j in range(n):
-            if not math.isclose(solution%training_input[i][j][0], training_input[i][j][1]):
+            si = training_input[i][j][0]
+            pi = training_input[i][j][1]
+            simulated_pi = (solution%si)*2*np.pi/si
+            if np.abs(simulated_pi-pi) > threshold_pi:
                 answer = (
                     f"not working in case {i} bc solution {solution} "
                     f"is not consistent with {j}th module:"
-                    f" si = {training_input[i][j][0]}  pi = {training_input[i][j][1]}"
+                    f" si = {si}  pi = {pi}"
                 )
                 return answer
+            answer = (
+                f"works in case {i} solution {solution} is consistent w {j}th module:"
+                f"si = {si}  pi = {pi}"
+            )
+            if(verbose):
+                print(answer)
+        print(f"solution for case {i} is good")
         return "Works!"
-            
+
+
+#print(phasesToLocation1Dtest(data, trial_num=1, PRECISION=0.5, TRY_LIMIT=100, n=5, verbose=True))
+print(phasesToLocation1Dtest(trial_num=100, PRECISION=0.2, TRY_LIMIT=1000, verbose=True))
---
+++ phaseSolutions.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+
+
+def phasesToLocation1D(data: np.ndarray, PRECISION=0.8, TRY_LIMIT=5) -> float:
+    '''
+    Convert phase data to a location in the 1D environment
+   
+    :param nparray data: nx2 np array, with wavelengths (s_i) in 1st col and phases (p_i) in 2nd col
+    :param float PRECISION: function will return when error in every module is less than this
+    :param int TRY_LIMIT: int, function will only attempt this many iterations before returning
+
+
+    :return float: -1 on failure, location otherwise
+    '''
+    s1: float = data[0][0]
+    p1: float = data[0][1]
+    tries = [(n + p1/(2*np.pi)) * s1 for n in range(TRY_LIMIT)]
+
+
+    for x in tries:
+        simulated_pi = np.array([(x%s)*2*np.pi/si])
+        try_ = np.array([np.cos(2 * np.pi/s * x - p) for s, p in data])
+        error = np.abs(try_ - 1)
+        if np.all(error < PRECISION):
+            return x
+
+
+    return -1
+
+
+if __name__ == '__main__':
+    data = np.array([
+        [11, 0.56],
+        [7, 1.8],
+        [5, 3.77],
+        [3, 4.18],
+        [2, 3.1415],
+    ])
+    print(f'{phasesToLocation1D(data):.2f}')
