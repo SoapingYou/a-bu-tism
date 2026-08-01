9573635839e5affb6c308b308ba22e99d615c0a7
FuzzyKoala <94997404+FuzzyKoala@users.noreply.github.com>
2026-08-01 13:41:23 -0400

    add profiling to optimized phase code

--- optimized_phases_to_location.py
+++ optimized_phases_to_location.py
@@ -4,12 +4,7 @@ import numpy as np
 from scipy.optimize import minimize
 
 Array = np.ndarray
-
-# %%
-# s_is = 25 * 1.65 ** np.arange(10)
-# real_p_i = np.array([6.0, 0.0, 5.15, 0.65, 4.2, 2.55, 1.55, 0.95, 0.55, 0.35]) # x = 123.8
-# p_is = np.round(real_p_i * 20) / 20
-
+TAU = 2 * np.pi
 # %%
 def _computeCosine(s: float, p: float, x: float) -> float:
     return np.cos(2 * np.pi / s * x - p)
@@ -25,7 +20,7 @@ def errorAtX(x: float, s_is: Array, p_is: Array) -> float:
 def sweepError(s_is: Array, p_is: Array, start=0, stop=22_000, step=2) -> np.ndarray:
     x = np.arange(start, stop, step)
     cosines = np.array(
-        [np.cos(2 * np.pi / s * x - p) for s, p in zip(s_is, p_is)]
+        [np.cos(TAU / s * x - p) for s, p in zip(s_is, p_is)]
     )
     error = np.abs(cosines - 1)
     return np.sum(error, axis=0)
@@ -56,7 +51,6 @@ def phasesToLocation1DOptimize(data: np.ndarray) -> float:
 
 def test():
     import math
-    TAU = np.pi * 2
     for _ in range(500):
         x = np.random.random() * 2_000 # [0, 500)
         s_is = 25 * 1.65 ** np.arange(10)
@@ -78,7 +72,51 @@ def test():
             break
     print()
 
+def test_time(trials=1_000):
+    import math, time
+    start = time.perf_counter()
+    for _ in range(trials):
+        x = np.random.random() * 2_000 # [0, 500)
+        s_is = 25 * 1.65 ** np.arange(10)
+    
+    
+        n = np.floor(x / s_is)
+        p_is = TAU * x / s_is - TAU * n
+        rounded_p_i = np.round(p_is * 20) / 20
+    
+        data = np.zeros((10,2))
+        data[:, 0] = s_is # 1st col
+        data[:, 1] = rounded_p_i # 2nd col
+    
+        x_guess = phasesToLocation1DOptimize(data)
+        if math.isclose(x, x_guess, rel_tol=25/20):
+            pass
+        else:
+            print(f'\nFAILED: {x=}, {x_guess=}, {data=}')
+            break
+    tot_time = time.perf_counter() - start
+    print(f'{tot_time=} sec, {tot_time / trials * 1000} msec/trial')
+
+
+def profile():
+    import cProfile
+    x = np.random.random() * 2_000 # [0, 500)
+    s_is = 25 * 1.65 ** np.arange(10)
+    
+    
+    n = np.floor(x / s_is)
+    p_is = TAU * x / s_is - TAU * n
+    rounded_p_i = np.round(p_is * 20) / 20
+    
+    data = np.zeros((10,2))
+    data[:, 0] = s_is # 1st col
+    data[:, 1] = rounded_p_i # 2nd col
+
+    x_guess = -1.
+    cProfile.runctx('x_guess = phasesToLocation1DOptimize(data)', globals(), locals(), 'profile.txt', 'cumtime')
+    
+
 if __name__ == '__main__':
-    test()
+    profile()
 
 
